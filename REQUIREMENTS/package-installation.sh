7ab29baf09e35a71b6412db50d917c050e57a11e
Faby <105114784+Fxby16@users.noreply.github.com>
2023-06-26 15:46:02 +0200

    Add files via upload

---
+++ REQUIREMENTS/package-installation.sh
@@ -0,0 +1,3 @@
+pip3 install --target ~/.local/lib/python3.10/site-packages spotipy
+pip3 install --target ~/.local/lib/python3.10/site-packages youtube-search
+pip3 install --target ~/.local/lib/python3.10/site-packages yt_dlp
